5332b393299cc05ba3085159ba692f76a8f22f04
Nisharga <kabir@nexlent.com>
2024-06-27 22:46:42 +0600

    feat: contact

--- src/app/(public)/(home)/components/HeroSection/HeroSection.tsx
+++ src/app/(public)/(home)/components/HeroSection/HeroSection.tsx
@@ -4,9 +4,10 @@ import { useWindowWidth } from '../../hooks';
 import { HERO_AREA, TYPE_WRITTER_WORDS } from '@/static/HeroArea';
 import { Wrapper } from '@/components';
 import { motion } from 'framer-motion';
-import { slideUp } from '@/components/animation';
+import { slideIn, slideUp } from '@/components/animation';
 import Button from '@/components/core/buttons/Button';
 import { Typewriter } from 'react-simple-typewriter';
+import Image from 'next/image';
 
 const HeroSection = () => {
     const { cta, subtitle, title, tagline, description, specialText } =
@@ -23,83 +24,108 @@ const HeroSection = () => {
         <div className='container '>
             <Wrapper
                 id='hero'
-                className='flex flex-col justify-center h-full min-h-screen gap-6 mt-12 xs:gap-7 xs:mt-0 mx-6 md:mx-10'
+                className='flex flex-col justify-center h-full min-h-screen gap-6 mt-12 xs:gap-7 xs:mt-0 mx-6 md:mx-10 '
             >
-                <motion.p
-                    variants={slideUp({ delay: getAnimationDelay(0) })}
-                    initial='hidden'
-                    animate='show'
-                    className='font-mono text-sm md:text-base text-accent'
-                >
-                    {subtitle}
-                </motion.p>
+                <div className='grid grid-cols-12'>
+                    <div className='col-span-12 lg:col-span-8 flex flex-col justify-center gap-6 xs:gap-7'>
+                        <motion.p
+                            variants={slideUp({ delay: getAnimationDelay(0) })}
+                            initial='hidden'
+                            animate='show'
+                            className='font-mono text-sm md:text-base text-accent'
+                        >
+                            {subtitle}
+                        </motion.p>
 
-                <div className='max-w-5xl text-4xl font-bold tracking-tighter md:text-7xl'>
-                    <motion.h1
-                        variants={slideUp({ delay: getAnimationDelay(1) })}
+                        <div className='max-w-5xl text-4xl font-bold tracking-tighter md:text-7xl  '>
+                            <motion.h1
+                                variants={slideUp({
+                                    delay: getAnimationDelay(1)
+                                })}
+                                initial='hidden'
+                                animate='show'
+                                className='text-slate-900 dark:text-slate-200 capitalize mb-2 leading-[1.1]'
+                            >
+                                {title}
+                            </motion.h1>
+                            <motion.h1
+                                variants={slideUp({
+                                    delay: getAnimationDelay(2)
+                                })}
+                                initial='hidden'
+                                animate='show'
+                                className='leading-[1.2]'
+                            >
+                                I am a{' '}
+                                <Typewriter
+                                    words={TYPE_WRITTER_WORDS}
+                                    loop={5}
+                                    cursor
+                                    cursorStyle='_'
+                                    typeSpeed={70}
+                                    deleteSpeed={50}
+                                    delaySpeed={1000}
+                                />
+                            </motion.h1>
+                        </div>
+
+                        <motion.p
+                            variants={slideUp({ delay: getAnimationDelay(3) })}
+                            initial='hidden'
+                            animate='show'
+                            className='max-w-xl text-base md:text-lg'
+                        ></motion.p>
+
+                        <motion.p
+                            variants={slideUp({ delay: getAnimationDelay(3) })}
+                            initial='hidden'
+                            animate='show'
+                            className='max-w-xl text-base md:text-lg'
+                        >
+                            {description}
+                        </motion.p>
+
+                        <motion.p
+                            variants={slideUp({ delay: getAnimationDelay(4) })}
+                            initial='hidden'
+                            animate='show'
+                            className='font-mono text-xs md:text-sm text-accent'
+                        >
+                            {specialText}
+                        </motion.p>
+
+                        <Button
+                            size='lg'
+                            type='link'
+                            variants={slideUp({ delay: getAnimationDelay(5) })}
+                            initial='hidden'
+                            animate='show'
+                            href={cta?.url ?? '#'}
+                            className={`mt-5 xs:mt-8 md:mt-10 ${
+                                cta.hideInDesktop ? 'md:hidden' : ''
+                            }`}
+                        >
+                            {cta.title}
+                        </Button>
+                    </div>
+                    <motion.div
+                        className='hidden lg:block lg:col-span-4'
+                        variants={slideIn({
+                            delay: getAnimationDelay(3)
+                        })}
                         initial='hidden'
                         animate='show'
-                        className='text-slate-900 dark:text-slate-200 capitalize mb-2 leading-[1.1]'
                     >
-                        {title}
-                    </motion.h1>
-                    <motion.h1
-                        variants={slideUp({ delay: getAnimationDelay(2) })}
-                        initial='hidden'
-                        animate='show'
-                        className='leading-[1.2]'
-                    >
-                        I am a{' '}
-                        <Typewriter
-                            words={TYPE_WRITTER_WORDS}
-                            loop={5}
-                            cursor
-                            cursorStyle='_'
-                            typeSpeed={70}
-                            deleteSpeed={50}
-                            delaySpeed={1000}
+                        <Image
+                            alt='profile'
+                            width={1000}
+                            height={1000}
+                            loading={'lazy'}
+                            className='w-full object-cover transition-all  '
+                            src='/walking-troll.svg'
                         />
-                    </motion.h1>
+                    </motion.div>
                 </div>
-
-                <motion.p
-                    variants={slideUp({ delay: getAnimationDelay(3) })}
-                    initial='hidden'
-                    animate='show'
-                    className='max-w-xl text-base md:text-lg'
-                ></motion.p>
-
-                <motion.p
-                    variants={slideUp({ delay: getAnimationDelay(3) })}
-                    initial='hidden'
-                    animate='show'
-                    className='max-w-xl text-base md:text-lg'
-                >
-                    {description}
-                </motion.p>
-
-                <motion.p
-                    variants={slideUp({ delay: getAnimationDelay(4) })}
-                    initial='hidden'
-                    animate='show'
-                    className='font-mono text-xs md:text-sm text-accent'
-                >
-                    {specialText}
-                </motion.p>
-
-                <Button
-                    size='lg'
-                    type='link'
-                    variants={slideUp({ delay: getAnimationDelay(5) })}
-                    initial='hidden'
-                    animate='show'
-                    href={cta?.url ?? '#'}
-                    className={`mt-5 xs:mt-8 md:mt-10 ${
-                        cta.hideInDesktop ? 'md:hidden' : ''
-                    }`}
-                >
-                    {cta.title}
-                </Button>
             </Wrapper>
         </div>
     );
